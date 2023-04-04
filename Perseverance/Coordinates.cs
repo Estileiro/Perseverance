979fee6d0c7f392874d0072273622a4b831d007d
Carlos <@DESKTOP-6U92M51>
2023-04-03 22:18:35 -0300

    - add new classes - abstraction - isolation - desing pattern - interface - try/catch

---
+++ Perseverance/Coordinates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perseverance
+{
+    public abstract class Coordinates
+    {
+        public char Direction { get; set; }
+        public string? NameDirection { get; set; }
+        public abstract void RobotDirection();
+
+    }
+}
--- Perseverance/Program.cs
+++ Perseverance/Program.cs
@@ -5,103 +5,42 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter the plateau size: ");
-        // Read the initial input
-        string input1 = Console.ReadLine();
-        string[] dimensions = input1.Split('x');
-        int plateauWidth = int.Parse(dimensions[0]);
-        int plateauHeight = int.Parse(dimensions[1]);
-
-        // Create the plateau
-        bool[,] plateau = new bool[plateauWidth, plateauHeight];
-
-        Console.Write("Enter the commands: ");
-        // Read the second input line containing the commands
-        string input2 = Console.ReadLine();
-
-        // Create the robot and set its initial position and facing direction
-        Robot robot = new Robot();
-        robot.X = 1;
-        robot.Y = 1;
-        robot.Direction = 'N';
-
-
-        foreach (char c in input2)
+        try
         {
-            if (c == 'L')
-            {
-                switch (robot.Direction)
-                {
-                    case 'N':
-                        robot.Direction = 'W';
-                        break;
-                    case 'S':
-                        robot.Direction = 'E';
-                        break;
-                    case 'E':
-                        robot.Direction = 'N';
-                        break;
-                    case 'W':
-                        robot.Direction = 'S';
-                        break;
-                }
-            }
-            else if (c == 'R')
-            {
-                switch (robot.Direction)
-                {
-                    case 'N':
-                        robot.Direction = 'E';
-                        break;
-                    case 'S':
-                        robot.Direction = 'W';
-                        break;
-                    case 'E':
-                        robot.Direction = 'S';
-                        break;
-                    case 'W':
-                        robot.Direction = 'N';
-                        break;
-                }
-            }
-            else if (c == 'F')
-            {
-                switch (robot.Direction)
-                {
-                    case 'N':
-                        robot.Y++;
-                        break;
-                    case 'S':
-                        robot.Y--;
-                        break;
-                    case 'E':
-                        robot.X++;
-                        break;
-                    case 'W':
-                        robot.X--;
-                        break;
-                }
-            }
-        }
+            Console.Write("Enter the plateau size: ");
+            // Read the initial input
+            string input1 = Console.ReadLine();
 
-        if (robot.Direction == 'N')
-        {
-            robot.NameDirection = "North";
-        }
-        else if (robot.Direction == 'S')
-        {
-            robot.NameDirection = "South";
-        }
-        else if (robot.Direction == 'W')
-        {
-            robot.NameDirection = "West";
-        }
-        else
-        {
-            robot.NameDirection = "East";
-        }
+            Plateu plateu = new Plateu();
 
-        Console.WriteLine($"Final position: ({robot.X}, {robot.Y}), {robot.NameDirection}");
+            plateu.dimensions = input1.Split('x');
+            plateu.plateauWidth = int.Parse(plateu.dimensions[0]);
+            plateu.plateauHeight = int.Parse(plateu.dimensions[1]);
+
+            // Create the plateau
+            bool[,] plateau = new bool[plateu.plateauWidth, plateu.plateauHeight];
+
+            Console.Write("Enter the commands: ");
+            // Read the second input line containing the commands
+            string input2 = Console.ReadLine();
+
+            // Create the robot and set its initial position and facing direction
+            Robot robot = new Robot();
+
+
+            robot.X = 1;
+            robot.Y = 1;
+            robot.Direction = 'N';
+            robot.RobotDirection(input2);
+
+            Console.WriteLine($"Final position: ({robot.X}, {robot.Y}), {robot.NameDirection}"); 
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Check your input again! Please restart the process from 0");
+        }
         Console.ReadKey();
     }
+
+
 }
